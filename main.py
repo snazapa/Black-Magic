1f1b1f701fe66341fab016a07a6ef64bd1022885
Travis Pressler <travisp471@gmail.com>
2020-03-02 22:02:30 -0500

    main loop

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ def main():
 
     screen = pygame.display.set_mode((240,180))
 
+    # define a variable to control the main loop
+    running = True
+
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
 if __name__=="__main__":
     main()
