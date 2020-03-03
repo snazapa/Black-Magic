da073876766be6313c7c3179e4c1f6a7e3745f25
Travis Pressler <travisp471@gmail.com>
2020-03-02 22:44:25 -0500

    the man himself

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ def main():
     # define a variable to control the main loop
     running = True
 
+    image = pygame.image.load("Paul_Benis.png")
+
     # main loop
     while running:
+        screen.blit(image, (50,50))
+        pygame.display.flip()
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
